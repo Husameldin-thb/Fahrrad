916a6119e477cd93d9baae5cb412bf8bc515f85e
JonasTHB <118432371+JonasTHB@users.noreply.github.com>
2023-01-24 15:22:04 +0100

    db added

--- js/db.js
+++ js/db.js
@@ -1,20 +1,25 @@
+let fs = require("fs");
 let sqlite3 = require('sqlite3').verbose();
 let filepath = "./webshop.db";
 
 function createDbConnection() {
-    let db = new sqlite3.Database(filepath, (error) => {
-        if (error) {
-            return console.error(error.message);
-        }
-        createTable(db);
-    });
-    console.log("Connection with Webshop established");
-    return db;
+    if (fs.existsSync(filepath)) {
+        return new sqlite3.Database(filepath);
+    } else {
+        let db = new sqlite3.Database(filepath, (error) => {
+            if (error) {
+                return console.error(error.message);
+            }
+            createTable(db);
+        });
+        console.log("Connection with Webshop established");
+        return db;
+    }
 }
 
 function createTable(db) {
     db.exec(`
-        CREATE TABLE IF NOT EXISTS customers (
+        CREATE TABLE customers (
             customer_id INTEGER NOT NULL PRIMARY KEY,
             name VARCHAR(50) NOT NULL,
             email VARCHAR(50) NOT NULL,
@@ -22,7 +27,7 @@ function createTable(db) {
         );
     `);
     db.exec(`
-        CREATE TABLE IF NOT EXISTS bookings (
+        CREATE TABLE bookings (
             bookings_id INTEGER NOT NULL,
             bike_id INTEGER NOT NULL,
             booking_date VARCHAR(50) NOT NULL,
