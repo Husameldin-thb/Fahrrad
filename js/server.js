11b4164063105a27c9a7a268d6a6f4f633cca770
JonasTHB <118432371+JonasTHB@users.noreply.github.com>
2023-01-25 23:58:41 +0100

    new

--- js/server.js
+++ js/server.js
@@ -336,7 +336,7 @@ app.post('/api/v1/booking', (req, res) => {
 			);
 			db.run(
 				`INSERT INTO bookings (bookings_id, bike_id, booking_date, number, customer_id) VALUES (?, ?, ?, ?, ?)`, 
-				[null, sessionHandler[2], sessionHandler[0], req.body.number, userid],
+				[null, sessionHandler[2], sessionHandler[0], req.body.number, row[0]],
 				function (error) {
 					if (error) {
 						console.error(error.message);
