147b3a2a31dbb5453a1b0cd3e38c11e32fb7d750
JonasTHB <118432371+JonasTHB@users.noreply.github.com>
2023-01-30 08:05:08 +0100

    new

--- static/buttons.js
+++ static/buttons.js
@@ -133,7 +133,7 @@ function showAlternatives() {
     axios.post('/api/v1/alternatives', )
     .then(function (res) {
         console.log(res);
-        console.log(session[0]);
+        console.log(session[0].data);
         //let obj = res.data.find(o => o.bike_id === 1)
         //console.log(obj);
         while(i < 7) {
